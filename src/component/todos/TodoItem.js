d6e0a6089627d1fda6b9853d422adbaf0448a2b1
sisilll10 <prisiliarumengan1004@gmail.com>
2021-11-16 23:50:57 +0800

    to do update

--- src/component/todos/TodoItem.js
+++ src/component/todos/TodoItem.js
@@ -5,7 +5,7 @@ function TodoItem({todo, dispatch}) {
     <div className = "container p-2 mt-2 bg-secondary form-check  d-flex align-items-center">
       <input type="checkbox"  onChange={() => dispatch({ type: 'flip', payload: {id : todo.id} })} />
       
-      <span style={{ textDecoration: todo.complete ? 'line-through' : 'none'}} >{todo.text}</span>
+      <span style={{ textDecoration: todo.complete ? 'line-through' : 'none'}} className="text-white" >{todo.text}</span>
       
       <button onClick={() => dispatch({ type: 'delete', payload: {id : todo.id}})} className="btn btn-danger ml-auto"><i class="fa-solid fa-trash"></i></button>
     </div>
